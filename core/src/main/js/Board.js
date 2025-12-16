6d445d85d7abc4f69f81f5ee5fb889b43c3eff88
SeworL <82595687+HIX4123@users.noreply.github.com>
2025-12-16 15:00:50 +0000

    implementing contains;piece

--- core/src/main/js/Board.js
+++ core/src/main/js/Board.js
@@ -16,4 +16,21 @@ export default class Board {
     isOccupied(s) {
         return Bitboard.contains(this.occupied, s)
     }
+
+    contains(...args) {
+        if (args.length == 1) {
+            let [p] = args;
+            return this.piece(p).length > 0;
+        } else if (args.length == 2) {
+            let [color, role] = args
+            return this.piece(color, role).length > 0
+        }
+    }
+
+    piece(...args) {
+        if (args.length == 1) {
+            let [p] = args
+            return this.piece()
+        }
+    }
 }
