1bef1a03fcb7c115edd0fa7a0bea1491b1c807f6
SeworL <82595687+HIX4123@users.noreply.github.com>
2025-12-23 14:01:22 +0000

    implementing Attacks.js

--- core/src/main/js/bitboard/Attacks.js
+++ core/src/main/js/bitboard/Attacks.js
@@ -1,5 +1,13 @@
 export default class Attacks {
-    constructor() {
-        
-    }
+
+    static all = 0xFFFFFFFFFFFFFFFFn;
+
+    static RANKS = new BigUint64Array(8);
+    static FILES = new BigUint64Array(8);
+    static BETWEEN = Array.from({ length: 64 }, () => new BigUint64Array(64));
+    static RAYS = Array.from({ length: 64 }, () => new BigUint64Array(64));
+
+    static ATTACKS = new BigUint64Array(88772);
+
+    
 }
